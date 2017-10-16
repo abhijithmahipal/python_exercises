0a28e77dce4dff97d8b1ccecc18751887813d119
abhijithmahipal <abhijithmahipal@gmail.com>
2017-10-16 15:16:34 +0530

    checked answers of 4th chapter by running it

---
+++ chapter4/ch4prob2.py
@@ -0,0 +1,15 @@
+try:
+    print "a"
+except:
+    print "b"
+else:
+    print "c"
+finally:
+    print "d"
+    
+'''
+Answers 
+a
+c
+d
+'''
---
+++ chapter4/ch4prob4.py
@@ -0,0 +1,19 @@
+def f():
+    try:
+        print "a"
+        return
+    except:
+        print "b"
+    else:
+        print "c"
+    finally:
+        print "d"
+
+f()
+
+'''
+Answers
+
+a
+d
+'''
