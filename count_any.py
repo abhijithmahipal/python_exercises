85036b3bc3b4d735de2e736beeefbf876f32eec8
abhijithmahipal <abhijithmahipal@gmail.com>
2017-10-31 10:28:59 +0530

    update

--- count_any.py
+++
@@ -1,9 +0,0 @@
-ch=raw_input("ENTER A CHARACTER TO BE COUNTER\n")
-st=raw_input("\nENTER A LETTER OR PARAGRAPH\n")
-count=0
-letter=''
-for letter in st:
-	if letter==ch:
-		count=count+1
-print "Count of "+ch+" in "+st+" is ",count
-
